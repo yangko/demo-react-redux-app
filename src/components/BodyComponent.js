933a0e202a9c5df2511d438ddfe03c7a93c42718
Yangko <yangko@hotmail.com>
2020-07-10 16:22:59 -0400

    code clear

--- src/components/BodyComponent.js
+++ src/components/BodyComponent.js
@@ -2,22 +2,8 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import Typography from '@material-ui/core/Typography';
-import Grid from '@material-ui/core/Grid';
-import StarIcon from '@material-ui/icons/StarBorder';
-//import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
-import TextField from '@material-ui/core/TextField';
-import Autocomplete from '@material-ui/lab/Autocomplete';
-import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import CardHeader from '@material-ui/core/CardHeader';
-import CardMedia from "@material-ui/core/CardMedia";
-import Button from "@material-ui/core/Button";
-import axios from "axios";
-import { loadCities } from '../actions';
 import { loadRestaurants } from '../actions';
-import { listRestaurantsBy, filterBy, sortBy } from '../actions';
 import Filter from './Filter/Filter';
 import RestaurantList from './RestaurantList/RestaurantList';
 import Loader from './Loader/Loader';
@@ -26,11 +12,14 @@ import './Filter/Filter.css';
 
 const HeroContent = () => (
   <React.Fragment>
-    <Container maxWidth="sm" component="main" className="heroContent">
-      <Typography component="h1" variant="h2" align="center" color="textPrimary" gutterBottom>
+    <Container maxWidth="lg" component="main" className="heroContent">
+      <Typography component="h1" variant="h3" align="center" color="textPrimary" gutterBottom>
       </Typography>
       <Typography variant="h4" align="center" color="textSecondary" component="p">
-        RESTful API, Search, Filtering and Sorting
+        A Demo of SPA (Single Page Application)
+      </Typography>
+      <Typography variant="h5" align="center" color="textSecondary" component="p">
+        Search restaurants in worldwide cities, filtering and sorting feature enabled. Using RESTful API, Redux store, Webpack, Material-UI.
       </Typography>
     </Container>
   </React.Fragment>
