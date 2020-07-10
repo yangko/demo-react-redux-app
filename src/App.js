95296b7228df54e14e724f31e850c04245d29ccf
Yangko <yangko@hotmail.com>
2020-07-10 13:00:10 -0400

    test to change App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+	    <h1>Welcome!</h1>      
+	    <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
